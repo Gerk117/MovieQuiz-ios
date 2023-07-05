cbb9453b4cd8a3e09e2fe26540eead73a4fb6ac2
Георгий Ксенодохов <gerk@MacBook-Pro-Georgij.local>
2023-07-05 03:37:01 +0300

    доработка проекта

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -138,4 +138,15 @@ struct QuizQuestion {
     let text: String
     let correctAnswer: Bool
 }
+struct QuizStepViewModel {
+    
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
 
